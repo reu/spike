b8bae9cd0520918688e524924c35bc2a22dcf62f
Rodrigo Navarro <rnavarro1@gmail.com>
2023-08-27 09:23:37 -0300

    Add examples.

--- examples/routing.rs
+++ examples/routing.rs
@@ -1,12 +1,11 @@
 use http::{Method, StatusCode};
-use spike::{get, post, put, IntoResponse, Router};
+use spike::{get, put, IntoResponse, Router};
 use touche::Server;
 
 fn main() -> std::io::Result<()> {
     let router = Router::new()
         .route("/hello", get(hello_world).post(hello_post))
         .route("/hello", put(put_hello_world).any(any_hello))
-        .route("/jesus", post(jesus))
         .route("/world", get(world));
 
     Server::bind("0.0.0.0:4444").serve(router)
@@ -30,8 +29,4 @@ fn any_hello(method: Method, body: String) -> impl IntoResponse {
 
 fn world(method: Method, body: String) -> impl IntoResponse {
     (StatusCode::CREATED, format!("World: {method} - {body}"))
-}
-
-fn jesus(method: Method) -> impl IntoResponse {
-    format!("Jesus {method}")
 }
